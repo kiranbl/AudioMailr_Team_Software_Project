ff1fa7c36af39bbb87ff9a089f9c809fb4104d35
Leyan AlAjmi <lmnalajmi1@sheffield.ac.uk>
2023-03-13 20:50:27 +0300

    signUp Utilities

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,18 +1,28 @@
-var mysql = require('mysql2');
+var mysql = require('mysql2/promise');
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Kiran123$',
-    database: 'AudioMailr',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, 
-    idleTimeout: 60000,
-    queueLimit: 0
-  });
+  host: 'localhost',
+  user: 'root',
+  password: 'Leyan_2002',
+  database: 'AudioMailr',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0
+});
 
+const connectToMySQL = async () => {
+  try {
+    await pool.getConnection();
+    console.log('MySQL database connected!');
+  } catch (err) {
+    console.log('MySQL database connection error!', err);
+    process.exit(1);
+  }
+};
 
+connectToMySQL().then();
 
 module.exports = pool;
