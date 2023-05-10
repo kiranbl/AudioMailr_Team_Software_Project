550bdb49eb9a7f6f9d5d1f4220b6c18580c66c2c
jianing_act22jd_UI <jdu35@sheffield.ac.uk>
2023-05-11 00:58:49 +0800

    Transfer the html code to react

---
+++ transfer html to react/mailitem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { getprettytime } from './utils/timeutils';
+import styles from "./css/maillist.css";
+//functions to handle long email addresses & subjects to keep the Emaillist steady
+const addFormat = (address) => {
+  const len = address.length;
+  if (len < 20) {
+    return address;
+  } else {
+    return address.slice(0, 20) + '...';
+  }
+};
+const addSubjectFormat = (subject) => {
+  const len = subject.length;
+  if (len < 20) {
+    return subject;
+  } else {
+    return subject.slice(0, 17) + '...';
+  }
+};
+//form of each item in Emaillist
+const MailItem = ({ selectedEmailID, mail, openmail }) => {
+  const display = mail.read === 'false' ? 'inline-block' : 'none';
+  return (
+    <li
+	className={`${selectedEmailID === mail.id ? styles.selecteditem : styles.mailitem} ${mail.read === "true" ? "" : styles.unread}`}
+      onClick={() => openmail(mail.id)}
+    >
+      <h5>{mail.tag === 'sent' ? addFormat(mail.address) : addFormat(mail.from)}</h5>
+      <div className={styles.circle} style={{ display: display }} />
+      <span>{getprettytime(mail.time)}</span>
+      <p>{addSubjectFormat(mail.subject)}</p>
+      <hr />
+    </li>
+  );
+};
+
+export default MailItem;
